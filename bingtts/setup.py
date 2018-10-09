e14ea0e7431b083ebde9f8bb40838bc8a3690325
JoseDev2018 <43177131+JoseDev2018@users.noreply.github.com>
2018-10-09 08:32:35 -0600

    Create setup.py

---
+++ bingtts/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BingTTS",
+    version="0.0.1",
+    author="Marvin Benítez",
+    author_email="mjbb20122@hotmail.com",
+    description="A wrapper to link python to the Bing API REST",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JoseDev2018/BingTTS",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
